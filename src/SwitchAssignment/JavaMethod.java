025f7328e59eb1d24661aac8e4d8f8f8d353932c
adityakarki <aditya_karki@hotmail.com>
2014-02-09 23:52:00 +0545

    Changes and assignment on marks

--- src/SwitchAssignment/JavaMethod.java
+++ src/SwitchAssignment/JavaMethod.java
@@ -4,5 +4,12 @@ package SwitchAssignment;
  * Created by aditya on 2/8/14.
  */
 public class JavaMethod {
+    public static void main (String args[]){
 
+        Marks marks = new Marks();
+        marks.getMarks();
+        marks.getTotal();
+        marks.geDivision();
+
+    }
 }
